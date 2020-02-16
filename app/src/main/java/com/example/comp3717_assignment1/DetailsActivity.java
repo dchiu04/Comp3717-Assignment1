f9f27ccac9abf2ece5e7ef7431b17b049319ca0c
debby <debbychiu@outlook.com>
2020-02-15 20:50:28 -0800

    we are done

--- app/src/main/java/com/example/comp3717_assignment1/DetailsActivity.java
+++ app/src/main/java/com/example/comp3717_assignment1/DetailsActivity.java
@@ -28,19 +28,19 @@ public class DetailsActivity extends AppCompatActivity {
         int i = (int) getIntent().getExtras().get("index");
 
         article_source = findViewById(R.id.article_source);
-        article_source.setText(ArticleArray.articles.get(i).getSource());
+        article_source.setText("Source: " + ArticleArray.articles.get(i).getSource());
 
         article_author = findViewById(R.id.article_author);
-        article_author.setText(ArticleArray.articles.get(i).getAuthor());
+        article_author.setText("Author: " + ArticleArray.articles.get(i).getAuthor());
 
         article_title = findViewById(R.id.article_title);
-        article_title.setText(ArticleArray.articles.get(i).title());
+        article_title.setText("Title: " + ArticleArray.articles.get(i).title());
 
         article_description = findViewById(R.id.article_description);
-        article_description.setText(ArticleArray.articles.get(i).getDescription());
+        article_description.setText("Description: " + ArticleArray.articles.get(i).getDescription());
 
         article_URL= findViewById(R.id.article_URL);
-        article_URL.setText(ArticleArray.articles.get(i).getUrl());
+        article_URL.setText("URL: " + ArticleArray.articles.get(i).getUrl());
 
         img = findViewById(R.id.img);
         String url_to_img = ArticleArray.articles.get(i).getUrlToImage();
@@ -50,9 +50,9 @@ public class DetailsActivity extends AppCompatActivity {
                 .into(img);
 
         article_publish_time = findViewById(R.id.article_publish);
-        article_publish_time.setText(ArticleArray.articles.get(i).getPublishedAt());
+        article_publish_time.setText("Publish time: " + ArticleArray.articles.get(i).getPublishedAt());
 
         article_content = findViewById(R.id.article_content);
-        article_content.setText(ArticleArray.articles.get(i).getContent());
+        article_content.setText("Content: " + ArticleArray.articles.get(i).getContent());
     }
 }
